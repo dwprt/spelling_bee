a02e82f46f438f0ffc36444c807cbaec3019af93
dwprt <dporter91@gmail.com>
2023-09-29 19:17:41 -0800

    9/29 Script work

--- spelling_bee.py
+++ spelling_bee.py
@@ -2,30 +2,37 @@ with open('enable2k/WORD.LST', 'r') as file:
     wordlist = file.readlines() 
 
 wordlist = [line.strip() for line in wordlist]
-print(wordlist[:20])
 
 vowels = ['a', 'e', 'i', 'o', 'u']
-print(vowels)
-
-#words_of_length = [word for word in wordlist if len(word) > 4]
-#print(words_of_length[:20])
 
 vowel_words = [word for word in wordlist if any(vowel in word for vowel in vowels)]
-print(vowel_words[:20])
+length_words = [word for word in vowel_words if len(word) >= 4 and len(word) <= 8]
 
-e_words = [word for word in vowel_words if 'e' in word]
-print(e_words[:10])
+#todays_letters = set(['n', 'x', 'd', 'g', 'i', 'e'])
 
-print(f'The length of my e_words list is {len(e_words)} words.')
+#print(f'The length of my e_words list is {len(e_words)} words.')
 
 # Writing a function
 
+#def filter_words(word_list, central_letter):
+#    filtered_list = [word for word in word_list if any(letter in word for letter in todays_letters) and central_letter in word]
+#    return filtered_list
+
 def filter_words(word_list, central_letter):
-    filtered_list = [word for word in word_list if any(vowel in word for vowel in vowels) and central_letter in word]
+    filtered_list = [word for word in word_list if central_letter in word and any(letter in word for letter in vowels)]
     return filtered_list
-    
+   
 central_letter = input("Please enter the central letter: ")
 
 filtered_results = filter_words(wordlist, central_letter)
 
 print(filtered_results[:10])
+
+lambda_results = filter(lambda word: any(vowel in word for vowel in vowels) and central_letter in word, wordlist)
+
+lambda_results_list = list(lambda_results)
+
+print(lambda_results_list[:10])
+
+print(filtered_results == lambda_results_list)
+
